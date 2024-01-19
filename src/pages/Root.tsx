8232b04acefdfaf575faea38cf151e51fde197e7
Sean Eric Thomas <set@seanericthomas.com>
2024-01-18 17:02:01 -0800

    Modify color & add shadow to page container

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -5,12 +5,14 @@ import Nav from "@components/Nav";
 
 export default function Root() {
   return (
-    <div className="text-brand-black relative flex min-h-[100svh] flex-col items-center bg-brand-light-gray">
-      <Nav />
-      <main className="flex w-full max-w-[1000px] flex-col items-center bg-brand-white p-2 h-full">
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="text-brand-black relative flex flex-col items-center min-h-[100svh] bg-brand-canvas">
+      <div className="shadow-sides max-w-[1000px] w-full bg-red-300 relative flex flex-col h-full items-center">
+        <Nav />
+        <main className="flex w-full max-w-[1000px] flex-col items-center bg-brand-white p-2 h-full">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        "sides": "-5px 0 5px -5px rgba(0, 0, 0, 0.8), 5px 0 5px -5px rgba(0, 0, 0, 0.8)",
+      },
       colors: {
         "brand-red": "#ee0000",
         "brand-black": "#333333",
+        "brand-canvas": "#f9f7f3",
         "brand-gray": "#e5e7eb",
         "brand-light-gray": "#f2f3f5",
         "brand-dark-gray": "#949aa1",
