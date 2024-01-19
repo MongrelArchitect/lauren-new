4374de9f84a1081598ac1532d075cff3fc601750
Sean Eric Thomas <set@seanericthomas.com>
2024-01-19 15:42:52 -0800

    Style contact page with image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import contactImage from "@assets/images/contact.jpg";
+
 export default function Contact() {
   return (
-    <div>CONTACT</div>
+    <div className="flex flex-col gap-2 w-full">
+      <h1 className="text-3xl">Contact</h1>
+      <p className="text-xl flex gap-1">
+        email:
+        <Link className="text-brand-red hover:underline" to="mailto:lmbass@roadrunner.com">lmbass@roadrunner.com</Link>
+      </p>
+      <img
+        alt=""
+        className="w-full border-2 border-brand-red p-2"
+        src={contactImage}
+      />
+    </div>
   );
 }
