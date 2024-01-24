39ad77c51209933c5a65f31d2e17abb5c66a3f61
Sean Eric Thomas <set@seanericthomas.com>
2024-01-23 21:44:06 -0800

    Tweak dashboard landing page

--- src/components/DashHome.tsx
+++ src/components/DashHome.tsx
@@ -5,9 +5,13 @@ export default function DashHome() {
   const user = useContext(UserContext);
 
   return (
-    <div className="h-full">
-      <h2 className="text-2xl">Welcome {user ? user.name : null}!</h2>
-      <p>Choose a menu item to get started.</p>
+    <div className="flex h-full flex-col gap-2">
+      <h2 className="w-full bg-brand-red p-2 text-3xl text-brand-white">
+        Welcome {user ? user.name : null}!
+      </h2>
+      <p className="text-xl">
+        Choose a section from the menu to view and edit its content.
+      </p>
     </div>
   );
 }
